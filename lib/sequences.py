bb0315a5407fc196efcc0002b9518a737a1b5b88
Alex <alex.mani@student.moringaschool.com>
2025-05-08 16:16:17 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,15 @@
 
 def print_fibonacci(length):
     pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return
+    elif n == 1:
+        print([0])
+        return
+
+    fib = [0, 1]
+    while len(fib) < n:
+        fib.append(fib[-1] + fib[-2])
+    print(fib)
